5ed8a488f6b84d0b0f57c0034f8b1fb3c92c80c1
hamnahift9 <hamnahift9@gmail.com>
2025-11-04 00:14:09 +0500

    Update main.lua

--- flappy/main.lua
+++ flappy/main.lua
@@ -4,6 +4,7 @@
 
     Author: Colton Ogden
     cogden@cs50.harvard.edu
+    Finished by Hamnah Iftikhar 
 
     A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple
     but effective gameplay mechanic of avoiding pipes indefinitely by just tapping
